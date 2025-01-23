86f5ffa530879d28ba55bdaecc6ddd60dd62213e
PAPAYA <dameningen@126.com>
2025-01-24 04:27:33 +0800

    basic functions

---
+++ Assets/Scripts/BubbleMasterScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BubbleMasterScript : MonoBehaviour
+{
+	#region SINGLETON
+	public static BubbleMasterScript me;
+	private void Awake()
+	{
+		me = this;
+	}
+	#endregion
+	public List<GameObject> bubbles;
+	private List<GameObject> shuffledList;
+	private bool bubblesPopulated;
+	private bool bubblesReady;
+	
+	public int amount_bomb;
+	private void Update()
+	{
+		// check if list populated
+		if (!bubblesPopulated &&
+			bubbles.Count == BubbleMakerScript.me.amount_col * BubbleMakerScript.me.amount_row)
+		{
+			bubblesPopulated = true;
+		}
+		// when list populated, put in items once
+		if (bubblesPopulated && !bubblesReady)
+		{
+			PutInBombs();
+			bubblesReady = true;
+		}
+	}
+	private void PutInBombs()
+	{
+		// shuffle list
+		shuffledList =  UtilityFunctions.me.ShuffleList(bubbles);
+		for (int i = 0; i < amount_bomb; i++)
+		{
+			shuffledList[i].GetComponent<BubbleScript>().bomb = true;
+		}
+	}
+}
---
+++ Assets/Scripts/BubbleScript.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleScript : MonoBehaviour
+{
+	public GameObject PSprefab_burst;
+	public bool bomb = false;
+	public int hp = 0;
+	public int rowNumber;
+	public int colNumber;
+	private void Update()
+	{
+		if (hp <= 0)
+		{
+			OnBurst();
+		}
+	}
+	private void OnMouseDown()
+	{
+		SpawnHurtPS();
+		hp -= 1;
+	}
+	private void OnBurst()
+	{
+		if (bomb)
+		{
+			Explode();
+		}
+		BubbleMasterScript.me.bubbles.Remove(gameObject);
+		gameObject.SetActive(false);
+	}
+	
+	
+	#region FOR UPGRADES
+	private void Explode() // destroy neighbour bubbles
+	{
+		foreach (var bubble in BubbleMasterScript.me.bubbles)
+		{
+			BubbleScript bs = bubble.GetComponent<BubbleScript>();
+			if ((bs.rowNumber == rowNumber && bs.colNumber == colNumber - 1) ||
+			(bs.rowNumber == rowNumber && bs.colNumber == colNumber + 1) ||
+			(bs.rowNumber == rowNumber - 1 && bs.colNumber == colNumber) ||
+			(bs.rowNumber == rowNumber + 1 && bs.colNumber == colNumber))
+			{
+				bs.DelayedDMGCaller(0.25f, 1);
+			}
+			if ((bs.rowNumber == rowNumber && bs.colNumber == colNumber - 2) ||
+			(bs.rowNumber == rowNumber && bs.colNumber == colNumber + 2) ||
+			(bs.rowNumber == rowNumber - 2&& bs.colNumber == colNumber) ||
+			(bs.rowNumber == rowNumber + 2 && bs.colNumber == colNumber))
+			{
+				bs.DelayedDMGCaller(0.5f, 1);
+			}
+		}
+	}
+	public void DelayedDMGCaller(float delayDuration, int dmgAmount) // used to start coroutine from another script
+	{
+		StartCoroutine(DelayedDMG(delayDuration, dmgAmount));
+	}
+	private IEnumerator DelayedDMG(float delayDuration, int dmgAmount) // create time differences between bursts
+	{
+		yield return new WaitForSeconds(delayDuration);
+		SpawnHurtPS();
+		hp -= dmgAmount;
+	}
+	#endregion
+
+	#region VFX
+	private void SpawnHurtPS()
+	{
+		Vector3 pos = new(transform.position.x, transform.position.y, transform.position.z - 0.2f);
+		Instantiate(PSprefab_burst, pos, Quaternion.identity);
+	}
+	#endregion
+}
