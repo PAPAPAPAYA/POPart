8b77bc6f5f63aece6e6b57f42de661294bea8986
Cooper Yang Windows <ty1037@nyu.edu>
2025-01-26 15:56:46 -0800

    assign mat

--- Assets/Scripts/ClearPlayerPrefsEditor.cs
+++ Assets/Scripts/ClearPlayerPrefsEditor.cs
@@ -1,17 +1,17 @@
-using UnityEditor;
-using UnityEngine;
+// using UnityEditor;
+// using UnityEngine;
 
-public class ClearPlayerPrefsEditor : Editor
-{
-   [MenuItem("Tools/Clear Player Preferences")]
-   public static void ClearPlayerPrefs()
-   {
-       if (EditorUtility.DisplayDialog("Clear Player Preferences",
-           "Are you sure you want to clear all player preferences? This action cannot be undone.", "Yes", "No"))
-       {
-           PlayerPrefs.DeleteAll();
-           PlayerPrefs.Save();
-           Debug.Log("Player preferences cleared.");
-       }
-   }
-}
+// public class ClearPlayerPrefsEditor : Editor
+// {
+//    [MenuItem("Tools/Clear Player Preferences")]
+//    public static void ClearPlayerPrefs()
+//    {
+//        if (EditorUtility.DisplayDialog("Clear Player Preferences",
+//            "Are you sure you want to clear all player preferences? This action cannot be undone.", "Yes", "No"))
+//        {
+//            PlayerPrefs.DeleteAll();
+//            PlayerPrefs.Save();
+//            Debug.Log("Player preferences cleared.");
+//        }
+//    }
+// }
